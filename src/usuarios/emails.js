5f1c1116ba4dd84db42a84153c942e2db5083a00
Pedro Bittencourt <pbittenc@gmail.com>
2021-11-01 19:18:20 -0300

    implementação de verificação por email

---
+++ src/usuarios/emails.js
@@ -0,0 +1,28 @@
+const nodemailer = require("nodemailer")
+
+class Email {
+    async enviaEmail() {
+        const contaTeste = await nodemailer.createTestAccount()
+        const transportador = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            auth: contaTeste,
+        })
+        const info = await transportador.sendMail(this)
+
+        console.log("URL: " + nodemailer.getTestMessageUrl(info))
+    }
+
+}
+
+class EmailVerificacao extends Email {
+    constructor(usuario, endereco){
+        super()
+        this.from = '"Blog do Código" <noreply@blogdocodigo.com.br>'
+        this.to = usuario.email
+        this.subject = "Verificação de E-mail"
+        this.text = `Olá! Verifique o seu e-mail aqui: ${endereco}`
+        this.html = `<h1>Olá!</h1> Verifique o seu e-mail <a href="${endereco}">aqui</a>.`
+    }
+}
+
+module.exports = { EmailVerificacao }
