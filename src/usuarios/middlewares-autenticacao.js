d28e322eaa87f5dc6e283d19cea8bee6a8addad6
Pedro Bittencourt <pbittenc@gmail.com>
2021-11-03 17:28:21 -0300

    centralização do tratamento de erros

--- src/usuarios/middlewares-autenticacao.js
+++ src/usuarios/middlewares-autenticacao.js
@@ -3,21 +3,13 @@ const Usuario = require('./usuarios-modelo')
 const tokens = require('./tokens')
 
 module.exports = {
-  local (req, res, next) {
+  local(req, res, next) {
     passport.authenticate(
       'local',
       { session: false },
       (erro, usuario, info) => {
-        if (erro && erro.name === 'InvalidArgumentError') {
-          return res.status(401).json({ erro: erro.message })
-        }
-
         if (erro) {
-          return res.status(500).json({ erro: erro.message })
-        }
-
-        if (!usuario) {
-          return res.status(401).json()
+          return next(erro)
         }
 
         req.user = usuario
@@ -27,27 +19,13 @@ module.exports = {
     )(req, res, next)
   },
 
-  bearer (req, res, next) {
+  bearer(req, res, next) {
     passport.authenticate(
       'bearer',
       { session: false },
       (erro, usuario, info) => {
-        if (erro && erro.name === 'JsonWebTokenError') {
-          return res.status(401).json({ erro: erro.message })
-        }
-
-        if (erro && erro.name === 'TokenExpiredError') {
-          return res
-            .status(401)
-            .json({ erro: erro.message, expiradoEm: erro.expiredAt })
-        }
-
         if (erro) {
-          return res.status(500).json({ erro: erro.message })
-        }
-
-        if (!usuario) {
-          return res.status(401).json()
+          return next(erro)
         }
 
         req.token = info.token
@@ -58,7 +36,7 @@ module.exports = {
     )(req, res, next)
   },
 
-  async refresh (req, res, next) {
+  async refresh(req, res, next) {
     try {
       const { refreshToken } = req.body
       const id = await tokens.refresh.verifica(refreshToken)
@@ -66,14 +44,11 @@ module.exports = {
       req.user = await Usuario.buscaPorId(id)
       return next()
     } catch (erro) {
-      if (erro.name === 'InvalidArgumentError') {
-        return res.status(401).json({ erro: erro.message })
-      }
-      return res.status(500).json({ erro: erro.message })
+      next(erro)
     }
   },
 
-  async verificacaoEmail (req, res, next) {
+  async verificacaoEmail(req, res, next) {
     try {
       const { token } = req.params
       const id = await tokens.verificacaoEmail.verifica(token)
@@ -81,18 +56,8 @@ module.exports = {
       req.user = usuario
       next()
     } catch (erro) {
-      if (erro.name === 'JsonWebTokenError') {
-        return res.status(401).json({ erro: erro.message })
-      }
+      next(erro)
 
-      if (erro.name === 'TokenExpiredError') {
-        return res.status(401).json({
-          erro: erro.message,
-          expiradoEm: erro.expiredAt
-        })
-      }
-
-      return res.status(500).json({ erro: erro.message })
     }
   }
 }
